adc59725477e59e38074e5c37263d9015121df8e
Alex Soper <alexsoper29@gmail.com>
2014-10-10 16:50:35 -0400

    Changing test function in Conway and deleting old code

--- 08--Conway-LITE.js
+++ 08--Conway-LITE.js
@@ -79,11 +79,7 @@ neighbors.forEach(conway);
 //TICK FUNCTION TO ADVANCE THE BOARD
 function tick(board){
   var newBoard = [];
-  /*for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board[i].length; j++) {
-      newBoard.push(conway(board[i][j], neighborsOf(board, i, j)));
-    }
-  }*/
+  
   board.forEach(function(value, index){
       value.forEach(function(x, y){
          newBoard.push(conway(board[index][y], neighborsOf(board, index, y)));
@@ -99,12 +95,7 @@ function tick(board){
 
 // TEST FUNCTION
 var assert = require('assert');
-//
-// function test(actual, expected, success){
-//     if (success === undefined) success = 'Success!';
-//     assert.strictEqual(actual, expected);
-//     console.log(success);
-// }
+
 
 describe('neighborsOf', function(){
   it('should return an array of neighbor values given a cell', function(){
@@ -124,43 +115,12 @@ describe('tick', function(){
       [ false, false, false ],
       [ false, false, false ],
       [ false, false, false ],
-    ]), [
-      [ false, false, false ],
-      [ false, false, false ],
-      [ false, false, false ],
-    ]);
+    ]), tick(board2));
   })
 })
 
-//FORMATTING FOR TICK TEST EXPERTLY ENGINEERED BY ARI
-// function tick_test(board) {
-//   var after = tick(board)
-//   console.log("-----------------------");
-//   console.log(board);
-//   console.log("-----------------------");
-//   console.log(after);
-//   console.log("-----------------------");
-// }
-
 //TEST VARIABLES AND BOARDS
-var actual1 = neighborsOf(board, 0, 0)
-var actual2 = neighborsOf(board, 0, 1)
-var actual3 = neighborsOf(board, 0, 2)
-var actual4 = neighborsOf(board, 1, 0)
-var actual5 = neighborsOf(board, 1, 1)
-var actual6 = neighborsOf(board, 1, 2)
-var actual7 = neighborsOf(board, 2, 0)
-var actual8 = neighborsOf(board, 2, 1)
-var actual9 = neighborsOf(board, 2, 2)
-var conway1 = conway(board[0][0], actual1)
-var conway2 = conway(board[0][1], actual2)
-var conway3 = conway(board[0][2], actual3)
-var conway4 = conway(board[1][0], actual4)
-var conway5 = conway(board[1][1], actual5)
-var conway6 = conway(board[1][2], actual6)
-var conway7 = conway(board[2][0], actual7)
-var conway8 = conway(board[2][1], actual8)
-var conway9 = conway(board[2][2], actual9)
+
 var board2   = [
                 [false, false, false],
                 [false, true , false],
@@ -191,14 +151,3 @@ var board7   = [
                 [true, true, true],
                 [true, true, true],
                 ]
-
-
-
-//TESTS
-// tick_test(board);
-// tick_test(board2);
-// tick_test(board3);
-// tick_test(board4);
-// tick_test(board5);
-// tick_test(board6);
-// tick_test(board7);
