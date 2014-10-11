2ed5253d82d44e205c887d85e6af69874a41267c
Alex Soper <alexsoper29@gmail.com>
2014-10-11 12:21:53 -0400

    Converting string calc to obejct oriented and adjusting test code

--- Dojos/dojo--2014-09-30.js
+++ Dojos/dojo--2014-09-30.js
@@ -18,47 +18,35 @@ var assert = require('chai').assert;
 
 var words = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10};
 
-function fromEnglish(word){
-  return words[word];
-};
-
-function add(wordA, wordB){
-  return words[wordA] + words[wordB];
-};
+var stringCalc = {
+  fromEnglish : function(wordA, wordB){
+    return words[wordA] + words[wordB];
+  }
+}
 
 describe('fromEnglish()', function(){
-  it('should convert "0" through "10"', function() {
-    assert.equal(fromEnglish("zero"), 0);
-    assert.equal(fromEnglish("one"),  1);
-    assert.equal(fromEnglish("two"), 2);
-    assert.equal(fromEnglish("three"),  3);
-    assert.equal(fromEnglish("four"), 4);
-    assert.equal(fromEnglish("five"),  5);
-    assert.equal(fromEnglish("six"), 6);
-    assert.equal(fromEnglish("seven"),  7);
-    assert.equal(fromEnglish("eight"), 8);
-    assert.equal(fromEnglish("nine"),  9);
-    assert.equal(fromEnglish("ten"),  10);
+  it('should return "0" when given "zero" and "zero"', function() {
+    assert.equal(stringCalc.fromEnglish('zero', 'zero'), 0);
   });
 });
 
-describe('add()', function(){
-  it('should add "0" to "0"', function() {
-    assert.equal(add("zero", "zero"), 0);
-  });
-  it('should add "1" to "2"', function() {
-    assert.equal(add("one", "two"), 3);
-  });
-  it('should add "3" to "4"', function() {
-    assert.equal(add("three", "four"), 7);
-  });
-  it('should add "5" to "6"', function() {
-    assert.equal(add("five", "six"), 11);
-  });
-  it('should add "7" to "8"', function() {
-    assert.equal(add("seven", "eight"), 15);
-  });
-  it('should add "9" to "10"', function() {
-    assert.equal(add("nine", "ten"), 19);
-  });
-});
+// describe('add()', function(){
+//   it('should add "0" to "0"', function() {
+//     assert.equal(add("zero", "zero"), 0);
+//   });
+//   it('should add "1" to "2"', function() {
+//     assert.equal(add("one", "two"), 3);
+//   });
+//   it('should add "3" to "4"', function() {
+//     assert.equal(add("three", "four"), 7);
+//   });
+//   it('should add "5" to "6"', function() {
+//     assert.equal(add("five", "six"), 11);
+//   });
+//   it('should add "7" to "8"', function() {
+//     assert.equal(add("seven", "eight"), 15);
+//   });
+//   it('should add "9" to "10"', function() {
+//     assert.equal(add("nine", "ten"), 19);
+//   });
+// });
