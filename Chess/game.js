9767237a2ed81517d2c225969900988895ba0255
Alex Soper <alexsoper29@gmail.com>
2014-10-19 21:12:04 -0400

    Fixed issue with pieces, reafctured tests, and removed some old code

--- Chess/game.js
+++ Chess/game.js
@@ -32,39 +32,12 @@
     @return String representation of Piece
       example: "Q" === String(new Piece("Queen", "white"))
       example: "r" === String(new Piece("Rook", "black"))
-*/
 
-//module.exports = Game;
-
-// var _ = require('lodash');
-//
-// // var boardBegin = [
-// //   R = {
-// //     name: 'Rook',
-// //     color: 'black',
-// //     position: [[0,0],[0,7]]
-// //   }
-// // ]
-//
-// var lodash = require('lodash');
-//
-/*
 var boardBegin = [
-  R = {
-    name: 'Rook',
-    color: 'black',
-    position: [[0,0],[0,7]]
-  },
+  R = { name: 'Rook', color: 'black', position: [[0,0],[0,7]] },
 
-  B = {
-    name: 'Bishop',
-    color: 'black',
-    position:
-  },
-  N = {
-    name: 'Knight',
-    color: 'black',
-    position:
+  B = { name: 'Bishop', color: 'black', position: },
+  N = { name: 'Knight', color: 'black', position:
   },
   K = {
     name: 'King',
@@ -114,28 +87,13 @@ var boardBegin = [
   },
 ];
 
+boardBegin.forEach(function(value, index){
+  pieces.push(boardBegin[index] = new Piece(boardBegin[index].name, boardBegin[index].color));
+  //console.log(value.position = boardBegin[index].position);
+  //console.log(Rook);
+});
+
 */
-//
-// var pieces = [];
-// console.log(pieces);
-//
-// function Piece(name, color){
-//  this.name = name;
-//  this.color = color;
-//  this.setPosition;
-// }//END Piece
-//
-// //var Q = new Piece('Queen', 'black' );
-// //console.log(Q);
-//
-// boardBegin.forEach(function(value, index){
-//   pieces.push(boardBegin[index] = new Piece(boardBegin[index].name, boardBegin[index].color));
-//   //console.log(value.position = boardBegin[index].position);
-//   //console.log(Rook);
-// });
-//
-// console.log(lodash.identity(R));
-//
 
 module.exports = Chess, Position, Piece;
 
@@ -151,40 +109,42 @@ var points = {
 
 
 function Chess(){
-  pieces = [];
+
+  this.pieces = [];
 
   moveTotal = 0;
 
-  var self = this;
   collection = [
-    [self.K, 'King'   , 'black',0,3]  , [self.k,  'King'  , 'white',7,4],
-    [self.Q, 'Queen'  , 'black',0,4]  , [self.q,  'Queen' , 'white',7,3],
-    [self.R1, 'Rook'  , 'black',0,0]  , [self.r1, 'Rook'  , 'white',7,0],
-    [self.R2, 'Rook'  , 'black',0,7]  , [self.r2, 'Rook'  , 'white',7,7],
-    [self.N1, 'Knight', 'black',0,1]  , [self.n1, 'Knight', 'white',7,1],
-    [self.N2, 'Knight', 'black',0,6]  , [self.n2, 'Knight', 'white',7,6],
-    [self.B1, 'Bishop', 'black',0,2]  , [self.b1, 'Bishop', 'white',7,2],
-    [self.B2, 'Bishop', 'black',0,5]  , [self.b2, 'Bishop', 'white',7,5],
-    [self.P1, 'Pawn'  , 'black',1,0]  , [self.p1, 'Pawn'  , 'white',6,0],
-    [self.P2, 'Pawn'  , 'black',1,1]  , [self.p2, 'Pawn'  , 'white',6,1],
-    [self.P3, 'Pawn'  , 'black',1,2]  , [self.p3, 'Pawn'  , 'white',6,2],
-    [self.P4, 'Pawn'  , 'black',1,3]  , [self.p4, 'Pawn'  , 'white',6,3],
-    [self.P5, 'Pawn'  , 'black',1,4]  , [self.p5, 'Pawn'  , 'white',6,4],
-    [self.P6, 'Pawn'  , 'black',1,5]  , [self.p6, 'Pawn'  , 'white',6,5],
-    [self.P7, 'Pawn'  , 'black',1,6]  , [self.p7, 'Pawn'  , 'white',6,6],
-    [self.P8, 'Pawn'  , 'black',1,7]  , [self.p8, 'Pawn'  , 'white',6,7],
+    [this.K, 'King'   , 'Black',0,3]  , [this.k,  'King'  , 'White',7,4],
+    [this.Q, 'Queen'  , 'Black',0,4]  , [this.q,  'Queen' , 'White',7,3],
+    [this.R1, 'Rook'  , 'Black',0,0]  , [this.r1, 'Rook'  , 'White',7,0],
+    [this.R2, 'Rook'  , 'Black',0,7]  , [this.r2, 'Rook'  , 'White',7,7],
+    [this.N1, 'Knight', 'Black',0,1]  , [this.n1, 'Knight', 'White',7,1],
+    [this.N2, 'Knight', 'Black',0,6]  , [this.n2, 'Knight', 'White',7,6],
+    [this.B1, 'Bishop', 'Black',0,2]  , [this.b1, 'Bishop', 'White',7,2],
+    [this.B2, 'Bishop', 'Black',0,5]  , [this.b2, 'Bishop', 'White',7,5],
+    [this.P1, 'Pawn'  , 'Black',1,0]  , [this.p1, 'Pawn'  , 'White',6,0],
+    [this.P2, 'Pawn'  , 'Black',1,1]  , [this.p2, 'Pawn'  , 'White',6,1],
+    [this.P3, 'Pawn'  , 'Black',1,2]  , [this.p3, 'Pawn'  , 'White',6,2],
+    [this.P4, 'Pawn'  , 'Black',1,3]  , [this.p4, 'Pawn'  , 'White',6,3],
+    [this.P5, 'Pawn'  , 'Black',1,4]  , [this.p5, 'Pawn'  , 'White',6,4],
+    [this.P6, 'Pawn'  , 'Black',1,5]  , [this.p6, 'Pawn'  , 'White',6,5],
+    [this.P7, 'Pawn'  , 'Black',1,6]  , [this.p7, 'Pawn'  , 'White',6,6],
+    [this.P8, 'Pawn'  , 'Black',1,7]  , [this.p8, 'Pawn'  , 'White',6,7],
   ]
+
+  self = this;
   collection.forEach(function(piece, index){
-      pieces.push(piece[0] = new Piece(piece[1], piece[2]))
-      piece[0].position = new Position(piece[3], piece[4])
+      self.pieces.push(piece[0] = new Piece(piece[1], piece[2]))
+      piece[0].setPosition([piece[3], piece[4]])
   })
 
 }//END Chess
 
 
 
-Chess.prototype.getPlayer = function(pieces){
-  if(pieces === pieces.toLowerCase()){
+Chess.prototype.getPlayer = function(item){
+  if(item === item.toLowerCase()){
      return 'White';
   }
      return 'Black';
@@ -195,12 +155,12 @@ Chess.prototype.getPlayer = function(pieces){
 //Move piece to destination and...?
 Chess.prototype.move = function (piece, destination){
   moveTotal++
-  pieces.forEach(function(value, index){
-    if (pieces[index].position[0] === piece[0] && pieces[index].position[1] === piece[1]){
+  self.pieces.forEach(function(value, index){
+    if (self.pieces[index].position[0] === piece[0] && self.pieces[index].position[1] === piece[1]){
 
-      pieces[index].position = destination;
+      self.pieces[index].position = destination;
 
-      console.log('Move ' + moveTotal + ': ' + pieces[index].color + ' ' + pieces[index].name + ' to '+ destination);
+      console.log('Move ' + moveTotal + ': ' + self.pieces[index].color + ' ' + self.pieces[index].name + ' to '+ destination);
       console.log(Chess.prototype.display());
     }
   })
@@ -219,6 +179,7 @@ Chess.prototype.opening = function (){
 }
 
 Chess.prototype.display = function(){
+
   Array.matrix = function(numrows, numcols, initial){
      var arr = [];
      for (var i = 0; i < numrows; ++i){
@@ -233,10 +194,10 @@ Chess.prototype.display = function(){
 
   var board = Array.matrix(8,8,0);
 
-  pieces.forEach(function(value, index){
-    var x = pieces[index].position[0];
-    var y = pieces[index].position[1];
-    board[x][y] = pieces[index].letter;
+  self.pieces.forEach(function(value, index){
+    var x = self.pieces[index].position[0];
+    var y = self.pieces[index].position[1];
+    board[x][y] = self.pieces[index].letter;
   })
   var spacer = '+---+---+---+---+---+---+---+---+\n';
     return spacer +
--- Chess/game_test.js
+++ Chess/game_test.js
@@ -7,28 +7,26 @@ var expect = require('chai').expect;
 var chess = new Chess;
 
 
-// it('should return "Queen"', function(){
-//   assert.equal(chess.Q.getName(), "Queen");
-// });
-// it('should return "black"', function(){
-//   assert.equal(chess.Q.getColor(), "Black")
-// })
-// it('should return "R" for "Black Rook"', function(){
-//   assert.equal(chess.R1.toString(), "R")
-// })
-// it('should return "n" for "White Knight"', function(){
-//   assert.equal(chess.n1.toString(), "n")
-// })
-// it('chess.k.getName should equal "King"', function(){
-//   expect(chess.k.getName()).to.equal("King");
-// })
-// it('should give me an "x = 0" and "y = 4" for Black Queen', function(){
-//   assert.deepEqual(chess.Q.position, [0 , 4])
-// })
-// it('should move P1 to "[3][0]" and clear its original position', function(){
-//   assert.deepEqual(chess.move([1,0],[3,0]) [0])
-// })
-// console.log(chess)
+it('should return "King"', function(){
+  assert.equal(chess.pieces[0].getName(), "King");
+ });
+it('should return "Black"', function(){
+  assert.equal(chess.pieces[0].getColor(), "Black")
+})
+it('should return "R" for "Black Rook"', function(){
+  assert.equal(chess.pieces[4].toString(), "R")
+})
+it('should return "n" for "White Knight"', function(){
+  assert.equal(chess.pieces[9].letter, "n")
+})
+it('chess.k.getName should equal "King"', function(){
+  expect(chess.pieces[0].getName()).to.equal("King");
+})
+it('should give me an "x = 0" and "y = 4" for Black Queen', function(){
+  assert.deepEqual(chess.pieces[2].position, [0 , 4])
+})
+
+console.log(chess)
 
 // console.log(chess.board)
 chess.opening();
